f5b12b6a079cb5fcb4f781dae0de1bda147e8efa
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-12 17:48:00 -0400

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int favNum = 90;
+    char myChar = 'i';
+    printf("Hello World\nThe backslash \"n\" creates a new line\n");
+    printf("%d thats a %s", 500, "number\n");
+    printf("%s", "thats a number\n");
+    printf("%d\n", favNum);
+    printf("%c", myChar);
+
+    return 0;
+}
