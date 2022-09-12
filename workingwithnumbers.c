129104a67111ed0ed1e5d4da8578b27fd6590995
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-12 18:00:01 -0400

    Create workingwithnumbers.c

---
+++ workingwithnumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num = 6;
+    printf("%f", 8 + 2.1);
+    printf("\n percent f is a float, percent d is an integer");
+    printf("\n%d", num);
+    printf("\n%f to the power of", pow(2, 3) );
+    printf("\n%f square root", sqrt(27) );
+    printf("\n%f rounds up", ceil(3.321) );
+    printf("\n%f rounds down\n we use  float with these because of decimal ", floor(3.321) );
+
+    return 0;
+}
