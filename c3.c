da022f7122ce27bbea055071b23b33204baf86e7
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-12 17:23:15 -0400

    Update c3.c

--- c3.c
+++ c3.c
@@ -3,10 +3,14 @@
 
 int main()
 {
-    printf("   /|\n");
-    printf("  / |\n");
-    printf(" /  |\n");
-    printf("/___|\n");
+
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("There once was a man named %s.\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
 
     return 0;
 }
