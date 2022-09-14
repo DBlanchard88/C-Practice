18bae9e1cdbb64afa8caf11a90fed31bc0dd1b85
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-13 20:05:41 -0400

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    printf("%d", 70);
+
+    return 0;
+}
+
+/* can also use command such as "const int num = 5"
+a constant is a variable that can not change. see print f example */
