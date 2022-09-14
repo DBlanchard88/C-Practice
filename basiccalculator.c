45880d30d287d1529628bd1210fcaa05139e28b5
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-13 21:09:51 -0400

    Create basiccalculator.c

---
+++ basiccalculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+    printf("enter first number: ");
+    scanf("%d", &num1);
+    printf("enter second number: ");
+    scanf("%d", &num2);
+    printf("Answere: %d", num1 + num2);
+
+
+    return 0;
+}
