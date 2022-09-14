5554a8fa8fc26d1847694734163138b065c7a346
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-13 21:30:43 -0400

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -3,10 +3,15 @@
 
 int main()
 {
+    
+    char color[10];
+    char plural_noun[20];
+    char noun[20];
 
-    printf("rosesd are red\n");
-    printf("violets are blue\n");
-    printf("i love you.\n");
+
+    printf("roses are %s\n", color, 20 );
+    printf("%s are blue\n", plural_noun, 20);
+    printf("i love %n.\n", noun, 20);
 
     return 0;
 }
