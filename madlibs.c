6f2c875f71d7a268ea2d713099e2a9c3118150e7
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-13 22:27:03 -0400

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -4,21 +4,21 @@
 int main()
 {
     
-    char color[50];
-    char pluralNoun[50];
-    char noun[50];
+    char color[100];
+    char pluralNoun[100];
+    char celebf[100];
+    char celebl[100];
 
     printf("Enter in a color: \n");
     scanf("%s", color);
     printf("Enter in a plural noun: \n");
     scanf("%s", pluralNoun);
-    printf("Enter in a noun: \n");
-    scanf("%s", noun);
-
-
+    printf("Enter in a celebrity: \n");
+    scanf("%s%s", celebf, celebl);
+    
     printf("roses are %s\n", color);
     printf("%s are blue\n", pluralNoun);
-    printf("i love %s.\n", noun);
+    printf("i love %s %s.", celebf, celebl);
 
     return 0;
 }
