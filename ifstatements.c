795e6c282ab6875af5ed3582aaf5062e0c6f84b9
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-17 21:25:52 -0400

    Update ifstatements.c

--- ifstatements.c
+++ ifstatements.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max(int num1, int num2){
+/* && is a logical operator.. allows to put more than one condition into a function*/
+
+
+int max(int num1, int num2, int num3){
     int result;
-    if(num1 > num2){
+    if(num1 >= num2 && num1 >= num3){
         result = num1;
-    } else {
+    } else if(num2 >= num1 && num2 >= num3){
         result = num2;
+    } else {
+        result = num3;
     }
     return result;
 }
 
 int main()
 {
-    printf("%d", max(40,10));
+    printf("%d", max(4, 2, 3));
 
     return 0;
 }
